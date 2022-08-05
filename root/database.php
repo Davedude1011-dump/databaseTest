363b499f1e71e2397c8b4e55ba9ac8222697b844
davedude1011 <105269128+davedude1011@users.noreply.github.com>
2022-08-05 13:50:46 +0100

    Delete root directory

--- root/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-         
-    $host = "localhost";
-    $username = "root";
-    $userpassword = "usbw";
-    $database_in_use = "test";
-    
-    $mysqli = new mysqli($host, $username, $userpassword, $database_in_use);
-?>
